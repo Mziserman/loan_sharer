0249ea7867b46a2898c373e118a928899ec04298
MZiserman <martinziserman@gmail.com>
2021-03-26 13:11:39 +0100

    nav / redir root

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def current_class(path)
+    return "current" if current_page?(path)
+    ""
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resource :standard, only: :show
   resource :linear, only: :show
 
+  root to: redirect('/standard')
 end
